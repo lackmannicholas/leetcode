9df1b3a4df9859339b5cc777502980aec19d4d4b
lackmannicholas <46652944+lackmannicholas@users.noreply.github.com>
2019-05-17 16:37:47 -0700

    Create LongestSubstringWithoutRepeatingCharacters.cs
    Given a string, find the length of the longest substring without repeating characters.

---
+++ LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int n = s.Length;
+            HashSet<char> set = new HashSet<char>();
+            int ans = 0, i = 0, j = 0;
+            while (i < n && j < n)
+            {
+                // try to extend the range [i, j]
+                if (!set.Contains(s[j]))
+                {
+                    set.Add(s[j++]);
+                    ans = Math.Max(ans, j - i);
+                }
+                else
+                {
+                    set.Remove(s[i++]);
+                }
+            }
+            return ans;
+    }
+}
