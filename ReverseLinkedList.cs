127898f545872abe57e6103a9c8cd2de6fe76285
lackmannicholas <46652944+lackmannicholas@users.noreply.github.com>
2019-05-17 16:41:43 -0700

    Create ReverseLinkedList.cs
    Reverse a singly linked list.

---
+++ ReverseLinkedList.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
